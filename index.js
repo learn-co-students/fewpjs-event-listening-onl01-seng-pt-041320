73c984297eed7a5bf2e0c8c444fd07e1d96e54f3
chica25 <patricia@patriciaco.com>
2020-11-04 22:14:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-function addingEventListener() {
+// function addingEventListener() {
 
-const input = document.getElementById('input');
-input.addEventListener('click', function(event) {
-  alert('I was clicked!');
-});
+// const input = document.getElementById('input');
+// input.addEventListener('click', function(event) {
+//   alert('I was clicked!');
+// });
 
-}
+// }
+
+function addingEventListener(){
+  const input = document.getElementById('input');
+  input.addEventListener('click', e => {
+    console.log(e.target);
+  });
+};
